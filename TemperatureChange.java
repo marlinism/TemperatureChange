e3a6a57baf3e1748492623de809151e5f476de03
marlinism <tuongbt1408@gmail.com>
2020-12-21 13:58:06 +0700

    Temperature Change System
    A program to transfer from Celcius degree into Fahrenheit degree or reverse

--- TemperatureChange.java
+++ TemperatureChange.java
@@ -4,12 +4,10 @@ public class TemperatureChange extends App{
 	
 	private double degree;
 	
-   //post: get name of the program
 	public void getName() {
 		System.out.println("\tTemperature Change");
 	}
 	
-   //post: get note(optional)
 	public void getNote() {
 		
 	}
@@ -22,21 +20,16 @@ public class TemperatureChange extends App{
 		setDegree(degree);
 	}
 	
-   //post: set the degree
 	public void setDegree(double degree) {
 		this.degree = degree;
 	}
 	
-   //pre: Fahrenheit degree
-   //post: transfer from Fahrenheit degree into Celcius degree
 	public double getCelcius (double degree) {
 		double celcius = 0;
 		celcius = 5*(degree-32)/9;
 		return celcius;
 	}
 	
-   //pre: Celcius degree
-   //post: transfer from Celcius degree into Fahrenheit degree
 	public double getFahrenheit (double degree) {
 		double fahrenheit = 0;
 		fahrenheit = degree*9/5 + 32.0;
